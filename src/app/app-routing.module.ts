c839581fa455618299e316916d052e5778d004e7
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-08 20:05:10 -0700

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,6 @@ import { HomeComponent } from './home/home/home.component';
 
 import { FundMaintenanceComponent } from './home/fund-maintenance/fund-maintenance.component';
 import { ReportComponent } from './home/report/report.component';
-import { FundModuleRouting } from './FEATURES/fund-module/fund.module.routing';
-import { FundListComponent } from './FEATURES/fund-module/fund-list/fund-list.component';
-import { FundAddComponent } from './FEATURES/fund-module/fund-add/fund-add.component';
 
 
 const routes: Routes = [
@@ -19,12 +16,7 @@ const routes: Routes = [
   { path: 'fundmtn', component: FundMaintenanceComponent },
   { path: 'rpt', component: ReportComponent  },
 
-  { 
-    path: 'fund-module', children : [ 
-      { path: 'get-all', component: FundListComponent },
-      { path: 'add-fund', component: FundAddComponent }
-    ]
-  }, //2. Fund Module - FundModuleRouting
+  { path: 'user-module', loadChildren:'./FEATURES/user-module/user.model#UserModule'}, 
 
   { path: '**', component: LoginComponent } //no match > login 
 ];
