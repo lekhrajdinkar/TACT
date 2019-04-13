4ce7e130e29b86c06075789d95db1a0e21b000a9
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-12 22:30:24 -0700

    Custom Progress bar

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
+import { ProgressBarComponent } from './custom-comp/progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProgressBarComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
-  exports: []
+  exports: [ProgressBarComponent]
   
 })
 export class TactCommonModule { }
