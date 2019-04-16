c164babeb51d6a92953fef2b10903da2887b6531
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-15 23:37:12 -0700

    table

--- src/app/FEATURES/fund-module/fof-table/fof-table.component.ts
+++ src/app/FEATURES/fund-module/fof-table/fof-table.component.ts
@@ -11,13 +11,20 @@ export class FofTableComponent implements OnInit {
 
   fofs: Fund[];
   cols: any[];
+  fundNumbers: {label : String, value : String}[] ; //= ['fgdf','fgfdg'];
+  created_bys: {label : String, value : String}[] ;
 
     constructor(private fundSrv: FundService) { }
 
     ngOnInit() {
         this.fundSrv.getAllFunds(1,100)
         .subscribe(
-          (data)=> {this.fofs = data.body; console.log(this.fofs)}
+          (data)=> {
+            this.fofs = data.body; 
+            console.log(this.fofs);
+            this.fundNumbers = this.fofs.map( fof => { return {label: fof.num, value: fof.num }}); //console.log(this.fundNumbers);
+            this.created_bys = this.fofs.map(fof =>  { return {label: fof.created_by, value: fof.created_by}} ); //console.log(this.created_bys);
+            }
         );
 
         this.cols = [
--- src/app/ngprime.module.ts
+++ src/app/ngprime.module.ts
@@ -3,9 +3,11 @@ import {SplitButtonModule} from 'primeng/splitbutton';
 import {PaginatorModule} from 'primeng/paginator';
 import {TableModule} from 'primeng/table';
 import {PanelModule} from 'primeng/panel';
+import {DropdownModule} from 'primeng/dropdown';
+import {MultiSelectModule} from 'primeng/multiselect';
 
 @NgModule({
-    imports:[SplitButtonModule,PaginatorModule, TableModule, PanelModule],
-    exports:[SplitButtonModule, PaginatorModule, TableModule, PanelModule]
+    imports:[SplitButtonModule,PaginatorModule, TableModule, PanelModule, DropdownModule, MultiSelectModule],
+    exports:[SplitButtonModule, PaginatorModule, TableModule, PanelModule, DropdownModule, MultiSelectModule]
 })
 export class PrimeNGModule {}
