8bd0cc102764ce98aed98aee73a505fae2b3f2bb
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-20 00:04:29 -0700

    progress bar - customizable

--- src/app/common/custom-comp/progress-bar/progress-bar.component.ts
+++ src/app/common/custom-comp/progress-bar/progress-bar.component.ts
@@ -8,10 +8,30 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProgressBarComponent implements OnInit {
 
   @Input() message: String;
+  @Input() bgColor: String = 'aqua';
+  @Input() color: String = 'black';
+  @Input('width') borderWidth: String = '1px';
+
+
   constructor() { }
 
   ngOnInit() {
     console.log(this.message);
+  }
+
+  getBgColor(){
+    console.log(this.bgColor);
+    
+    return {
+      "background-color": this.bgColor
+    };
+  }
+
+  getborder(){
+    return{
+      "border-color" : this.color ,
+      "border-width": this.borderWidth
+    }
   }
 
 }
