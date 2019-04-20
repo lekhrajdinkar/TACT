ee37a52f6cfb6b493f11b2acf67d4244121567e9
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-19 19:37:55 -0700

    login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,60 +7,58 @@ import { routingAminTriggerEnterLeft } from 'src/app/common/tact.anim-1';
 
 @Component({
   selector: 'login',
-  templateUrl: './login2.component.html',
+  templateUrl: './login.TD.component.html',
   styleUrls: ['./login.component.css'],
   animations: [routingAminTriggerEnterLeft]
 })
 export class LoginComponent implements OnInit {
 
-  loginModel : LoginModel = { username : 'lekhraj', password: 'lekh'};
-  xyz;
-  disable = true;
+
 
   //view form before submit
-  @ViewChild('f') loginForm : NgForm;
+  @ViewChild('loginForm') loginForm : NgForm;
 
   @HostBinding('@routingAminTriggerEnterLeft') routingAminTriggerEnterLeft = true;
-  form: FormGroup;
+  
   inProgress = false;
-  constructor(
-      //private fb:FormBuilder, 
-      private router:Router, private authSrv : AuthService) {
 
-      // this.form = fb.group({
-      //     email: ['tact_data_entry', [Validators.required]],
-      //     password: ['tact_data_entry', [Validators.required]]
-      // });
-
-  }
+  constructor(private router:Router, private authSrv : AuthService) {}
 
   ngOnInit() {
     console.log(this.loginForm);
   }
 
-
-  login(u,p) {
+  loginTD() {
     this.inProgress = true;
-    this.authSrv.authorize(u,p);
   }
 
-  clear(){
-    console.log("clear feilds...")
-  }
+ //Template 1
+//  login(u,p) {
+//   this.inProgress = true;
+//   this.authSrv.authorize(u,p);
+// }
 
-  //template 2
- print(event){
-   console.log(event.target.value);
-   this.xyz = event.target.value;
- }
+// //-------------------
 
- submit(f: any){ //ElementRef or NgForm
-   console.log(f);
-   this.disable = !f.valid ;
- }
+//template 2
+// loginModel : LoginModel = { username : 'lekhraj', password: 'lekh'};
+// disable = true;
+//  print(event){
+//    console.log(event.target.value);
+//    //this.xyz = event.target.value;
+//  }
 
- f3Input(f3: NgModel){
-   console.log(f3.valid, f3.value);
- }
+//  submit(f: any){ //ElementRef or NgForm
+//    console.log(f);
+//    this.disable = !f.valid ;
+//  }
+
+//  f3Input(f3: NgModel){
+//    console.log(f3.valid, f3.value);
+//  }
+
+//  clear(){
+//   console.log("clear feilds...")
+// }
 
 }
--- src/app/common/custom-comp/progress-bar/progress-bar.component.ts
+++ src/app/common/custom-comp/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressBarComponent implements OnInit {
 
+  @Input() message: String;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.message);
   }
 
 }
