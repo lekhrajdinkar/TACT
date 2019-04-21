d83a950f9aad49777ff34583e8abb400f8c48258
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-20 17:57:06 -0700

    subscribe to store for auth status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, noop } from 'rxjs';
 import { AuthService, UNKNOWN_USER } from './SERVICE/auth-service.service';
 import { map } from 'rxjs/operators';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { TactState } from './reducers';
 
 @Component({
   selector: 'app-root',
@@ -16,15 +18,25 @@ export class AppComponent implements OnInit{
 
   isLoggedIn$ : Observable<boolean> ;
 
-  constructor(private authSrv : AuthService, private router : Router){
+  constructor(private authSrv : AuthService, private router : Router ,
+    private store : Store<TactState>){
 
   }
 
   ngOnInit(): void {
     //transform one Obs to another.
-   this.isLoggedIn$ = this.authSrv.user$.pipe(
-     map (user => {console.log(user); return user !==  UNKNOWN_USER; })
-     ); 
+
+    //A. service approach
+  //  this.isLoggedIn$ = this.authSrv.user$.pipe(
+  //    map (user => {console.log(user); return user !==  UNKNOWN_USER; })
+  //    ); 
+
+  //B. store approach
+     this.isLoggedIn$ = this.store
+     .pipe(
+       map(state => state.authState.isLoggedIn) 
+      )
+     //.subscribe(noop);
   }
 
   logout(){
